dfb39de107e75fe4b52412cd9946a61e2d2b7dbe
Edward Marcheselli <eamarcheselli@gmail.com>
2023-03-16 19:50:59 -0400

    add page change function

--- main.js
+++ main.js
@@ -7,60 +7,65 @@
 var fadecounter = 0;
 var scrollEnabled = true;
 var slideDisp = document.getElementById('slide')
-slideDisp.counter = 0;
+slideDisp.counter = 1;
+slidechange()
 
-setTimeout(() => {
-    slideDisp.counter += 1;
-    slidechange();
-    console.log(slideDisp.counter)
-}, 1000);
-
-window.onwheel = function(e) {
-  if (e.deltaY > 20 && scrollEnabled == true && slideDisp.counter < 3) {
-    console.log('reveal');
-    scrollEnabled = false;
-    slidechange()
-    slideDisp.counter += 1;
-    scrollEnabled = true;
-    /* setTimeout(() => {
-      scrollEnabled = true;
-    }, 500); */
-  }
-  if (e.deltaY < -20 && scrollEnabled == true && slideDisp.counter > 0) {
-    console.log('reverse reveal');
-    scrollEnabled = false;
-    slidechange()
-    slideDisp.counter -= 1;
-    scrollEnabled = true;
-    /* setTimeout(() => {
-      scrollEnabled = true;
-    }, 500); */
-  }
+function pagechange(x) {
+  slideDisp.counter = x;
+  slidechange();
 }
+
+// setTimeout(() => {
+//     slideDisp.counter += 1;
+//     slidechange();
+//     console.log(slideDisp.counter)
+// }, 1000);
+
+// window.onwheel = function(e) {
+//   if (e.deltaY > 20 && scrollEnabled == true && slideDisp.counter < 3) {
+//     console.log('reveal');
+//     scrollEnabled = false;
+//     slidechange()
+//     slideDisp.counter += 1;
+//     scrollEnabled = true;
+//     /* setTimeout(() => {
+//       scrollEnabled = true;
+//     }, 500); */
+//   }
+//   if (e.deltaY < -20 && scrollEnabled == true && slideDisp.counter > 0) {
+//     console.log('reverse reveal');
+//     scrollEnabled = false;
+//     slidechange()
+//     slideDisp.counter -= 1;
+//     scrollEnabled = true;
+//     /* setTimeout(() => {
+//       scrollEnabled = true;
+//     }, 500); */
+//   }
+// }
 
 function slidechange() {
   switch (slideDisp.counter) {
     case 1:
-      $(() => {$("#slide").load("intro.html")})
-      console.log(slideDisp.counter)
+      $(() => {$("#slide").load("intro.html")});
+      console.log(slideDisp.counter);
       break;
     case 2:
-      $(() => {$("#slide").load("about.html")})
+      $(() => {$("#slide").load("projects.html")});
       //slideDisp.classList.toggle('fade');
-      console.log(slideDisp.counter)
+      console.log(slideDisp.counter);
       break;
     case 3:
-      $(() => {$("#slide").load("projects.html")})
-      console.log(slideDisp.counter)
+      $(() => {$("#slide").load("experience.html")});
+      console.log(slideDisp.counter);
       break;
     case 4:
-      slideDisp.innerHTML = '';
-      console.log(slideDisp.counter)
+      $(() => {$("#slide").load("about.html")});
+      console.log(slideDisp.counter);
       break;
     case 5:
       slideDisp.innerHTML = '';
-      console.log(slideDisp.counter)
+      console.log(slideDisp.counter);
       break;
   }
-
 }
